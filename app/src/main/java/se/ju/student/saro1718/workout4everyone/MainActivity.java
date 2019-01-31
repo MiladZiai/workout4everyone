9b0a699603ac3c508d98815c36712c60153cda0d
tobbin2 <robin_sauma@hotmail.com>
2019-01-31 14:55:46 +0100

    home page gui done, code still not done

--- app/src/main/java/se/ju/student/saro1718/workout4everyone/MainActivity.java
+++ app/src/main/java/se/ju/student/saro1718/workout4everyone/MainActivity.java
@@ -1,5 +1,6 @@
 package se.ju.student.saro1718.workout4everyone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    //view all workouts button clicked
+    public void viewAllWorkoutsButtonClicked(View view){
+        System.out.println("view all clicked");
+        Intent intent = new Intent(this, viewAllWorkoutsActivity.class);
+        startActivity(intent);
+    }
+
+    //create workout button clicked
+    public void createWorkoutButtonClicked(View view){
+        System.out.println("create clicked");
+    }
+
+    //view favorite workouts button clicked
+    public void viewFavoriteWorkoutsButtonClicked(View view){
+        System.out.println("favorite clicked");
     }
 
 }
