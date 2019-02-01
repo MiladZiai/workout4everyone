0415b12f69cd43f2c14d5473646b83bf392b1617
Milad Ziai <rami1700@student.ju.se>
2019-02-01 22:52:03 +0100

    Add pic functionality done!

--- app/src/main/java/se/ju/student/saro1718/workout4everyone/MainActivity.java
+++ app/src/main/java/se/ju/student/saro1718/workout4everyone/MainActivity.java
@@ -2,10 +2,7 @@ package se.ju.student.saro1718.workout4everyone;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     //view all workouts button clicked
     public void viewAllWorkoutsButtonClicked(View view){
-        System.out.println("view all clicked");
         Intent intent = new Intent(this, viewAllWorkoutsActivity.class);
         startActivity(intent);
     }
 
     //create workout button clicked
     public void createWorkoutButtonClicked(View view){
+
         System.out.println("create clicked");
+        Intent intent = new Intent(this, createWorkoutActivity.class);
+        startActivity(intent);
+
+
     }
 
     //view favorite workouts button clicked
