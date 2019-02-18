77df30f684b0a8a02483d4a00903d847c4c1e28f
tobbin2 <robin_sauma@hotmail.com>
2019-02-18 20:14:22 +0100

    trying to use firebase2

--- app/src/main/java/se/ju/student/saro1718/workout4everyone/profileFragment.java
+++ app/src/main/java/se/ju/student/saro1718/workout4everyone/profileFragment.java
@@ -1,5 +1,6 @@
 package se.ju.student.saro1718.workout4everyone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,28 +9,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+//firebase
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 import static se.ju.student.saro1718.workout4everyone.MainActivity.database;
 
 public class profileFragment extends Fragment {
 
+    FacebookLoginSupporter facebookLoginSupporter;
+    LoginButton loginButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
         FirebaseUser user = database.getFirebaseAuth().getCurrentUser();
+        
         if(user != null) {
             return inflater.inflate(R.layout.fragment_profile_logged_in, container, false);
         }else{
+            System.out.println("1");
+            loginButton = (LoginButton) loginButton.findViewById(R.id.facebook_login_button);
+            System.out.println("1");
+            facebookLoginSupporter = new FacebookLoginSupporter(loginButton);
             return inflater.inflate(R.layout.fragment_profile_sign_in,container,false);
         }
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        facebookLoginSupporter.callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
+    public void signOut(){
+        AuthUI.getInstance()
+                .signOut(this.getContext())
+            .addOnCompleteListener(new OnCompleteListener<Void>() {
+            public void onComplete(@NonNull Task<Void> task) {
+                // ...
+            }
+        });
     }
 
 }
